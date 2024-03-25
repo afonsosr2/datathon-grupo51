3f6a868869ffefc858689b515958c7fb55c16f01
Diego Lima <66536050+dlcerqueira@users.noreply.github.com>
2024-03-24 21:53:49 -0300

    Update home-passos.py

--- pages/home-passos.py
+++ pages/home-passos.py
@@ -16,7 +16,7 @@ with colunas_1 [1]:
 
 colunas_2 = st.columns(2, gap="large")
 with colunas_2 [0]:
-            st.image("images\metodologia.jpg"width= 250,use_column_width=True)
+            st.image("images\metodologia.jpg",width= 250,use_column_width=True)
             
 
 with colunas_2 [1]:
