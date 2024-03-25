1376c2f966dc9b20faf25688cf8696cd4168e8fa
Diego Lima <66536050+dlcerqueira@users.noreply.github.com>
2024-03-24 21:53:24 -0300

    Update home-passos.py

--- pages/home-passos.py
+++ pages/home-passos.py
@@ -12,11 +12,11 @@ with colunas_1 [0]:
             atividades extracurriculares de finais de semana e bolsas de estudos no colégio particular, em cursos técnicos e de graduação..""")
 
 with colunas_1 [1]:
-            st.image("images\Ong-Passos-Magicos.jpeg",width= 250,use_column_width=True")
+            st.image("images\Ong-Passos-Magicos.jpeg",width= 250,use_column_width=True)
 
 colunas_2 = st.columns(2, gap="large")
 with colunas_2 [0]:
-            st.image("images\metodologia.jpg"width= 250,use_column_width=True")
+            st.image("images\metodologia.jpg"width= 250,use_column_width=True)
             
 
 with colunas_2 [1]:
@@ -45,7 +45,7 @@ with colunas_3 [0]:
             
 colunas_3 = st.columns(2, gap="large")
 with colunas_3 [1]:
-            st.image("images\alunos-pac.jpg",width= 250,use_column_width=True")
+            st.image("images\alunos-pac.jpg",width= 250,use_column_width=True)
 
 
 
